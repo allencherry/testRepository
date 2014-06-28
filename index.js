3abf9829f13413890c1000c5c09c246872bae7c5
allencherry <allenhanchenlong@163.com>
2014-06-28 14:21:07 +0800

    哈哈

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("hello")vzdcvdzfvz
+//4.数据类型
+
+/**
+学习要点
+1.typeof操作符
+2.Undefined类型
+3.Null类型
+4.Booleanleix
+5.Number类型
+6.Stringleix
+7.Object类型
+公六种数据及类型五种简单数据类型一种复杂类型
+注意Function也可以用typeof操作符
+typeof用来检测变量的数据类型
+*/
+var box = 'allen';
+alert(typeof box);
+alert(typeof 'allen');
